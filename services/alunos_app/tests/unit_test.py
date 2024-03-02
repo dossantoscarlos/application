15dd9a8755068bb063c75db2ccbea9a9e7eb2f25
Carlos Eduardo <eduardo191949@hotmail.com>
2024-03-02 19:19:52 -0300

    release: removido serviço com python

--- services/alunos_app/tests/unit_test.py
+++
@@ -1,5 +0,0 @@
-from src.models.responsavel import Responsavel
-
-def test_responsavel_parentesco():
-    resp = Responsavel(nome="carlos" , sobrenome="santos", parentesco='Pai', cpf='0434343')
-    assert "Pai" == resp.parentesco
